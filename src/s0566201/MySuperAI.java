aabe1afd04a12c009640b53e80df774e1d0658ac
tobihoffmann <38102896+tobihoffmann@users.noreply.github.com>
2020-04-30 18:41:29 +0200

    Different circle treatment (#2)
    * align/arrive
    
    * align/arrive
    
    * initial steps for working with vectors
    
    * added calculation of direction vector, introduced some vectors to MySuperAI
    
    * reintroduced own vec2 classes
    
    * some stuff in align
    
    * tuned the car
    
    * this works
    
    Co-authored-by: jakobhes <jheschl95@gmail.com>

--- src/s0566201/MySuperAI.java
+++ src/s0566201/MySuperAI.java
@@ -1,7 +1,5 @@
 package s0566201;
 
-// TODO: Frage: wie sollen wir das package nennen wenn wir beide dran arbeiten???
-
 import static org.lwjgl.opengl.GL11.*;
 
 import lenz.htw.ai4g.ai.AI;
@@ -14,6 +12,20 @@ import java.awt.*;
 
 public class MySuperAI extends AI{
 
+    public Vec2 carPosition;
+    public float carOrientation;
+    public Vec2 checkpointPosition;
+    public Vec2 carToCheckpoint;
+    public Vec2 pointToRight;
+    public float checkpointOrientation;
+    public float steeringStrength;
+    public float steeringThreshold;
+    public float steeringStrengthThreshhold;
+    public float rotationToComplete;
+    float accelaration;
+
+    public float rotationVelocity;
+
     public MySuperAI (Info info) {
         super(info);
         //enlistForTournament(566201); //fuer Abgabe
@@ -24,42 +36,102 @@ public class MySuperAI extends AI{
 
     @Override
     public String getName() {
-        return "Jakob Heschl, Tobi Hoffmann";
+        return "Jakobi";
     }
 
     @Override
     public DriverAction update(boolean wasResetAfterCollision) {
-        // align
-        info.getX(); // meine Position
-        info.getY();
-        info.getCurrentCheckpoint(); // Zielposition
-        info.getOrientation(); // Blickrichtung zwischen -PI und +PI
-        info.getAngularVelocity(); // aktuelle Drehgeschwindigkeit
+
+
+        // Car Position as Vec2
+        carPosition = new Vec2(info.getX(), info.getY());
+
+        // Orientation of the car (as an Angle)
+        carOrientation = info.getOrientation();
+
+        // Position of the Current Checkpoint as Vec2
+        checkpointPosition = new Vec2((float)info.getCurrentCheckpoint().getX(), (float)info.getCurrentCheckpoint().getY());
+
+        // Direction vector from car to current checkpoint
+        carToCheckpoint = MathVec.direction(carPosition, checkpointPosition);
+
+        // A vector with an angle of 0
+        pointToRight = new Vec2(1,0);
+
+        // Orientation of direction vector from car to checkpoint (as an Angle)
+        checkpointOrientation = (carToCheckpoint.b > 0) ? MathVec.angle(pointToRight, carToCheckpoint) : - MathVec.angle(pointToRight, carToCheckpoint);
+
+        // Threshold of orientation alignment
+        steeringThreshold = 0.05f;
+
+        // Threshold of steering strength
+        steeringStrengthThreshhold = (float)Math.PI/2;
+
+        // current rotation velocity
+        rotationVelocity = info.getAngularVelocity();
+
+
+        rotationToComplete = Math.abs(checkpointOrientation - carOrientation);
+
+        if(rotationToComplete < steeringThreshold) {
+            // don't turn
+            steeringStrength = 0;
+        } else {
+            // take a turn
+            if (rotationToComplete < steeringStrengthThreshhold) {
+                //lower the steering strength
+                steeringStrength = (checkpointOrientation - carOrientation) * info.getMaxAbsoluteAngularVelocity() / steeringStrengthThreshhold;
+            } else {
+                // maximum steering strength
+                steeringStrength = (steeringStrength > 0) ? info.getMaxAbsoluteAngularVelocity() : -info.getMaxAbsoluteAngularVelocity();
+            }
+        }
+
+        float absoluteSteeringStrength = steeringStrength - info.getAngularVelocity();
+
+
+        if( absoluteSteeringStrength >= 0 && absoluteSteeringStrength > info.getMaxAbsoluteAngularAcceleration()) {
+            absoluteSteeringStrength = info.getMaxAbsoluteAngularAcceleration();
+        } else if (Math.abs(absoluteSteeringStrength) > info.getMaxAbsoluteAngularAcceleration()) {
+            absoluteSteeringStrength = -info.getMaxAbsoluteAngularAcceleration();
+        }
+
+
 
         Track track = info.getTrack();
         track.getWidth();
         track.getHeight();
         Polygon[] obstacles = track.getObstacles(); //(Oberflaeche der) Hindernisse
         Polygon obs = obstacles[0];
-
-        obs.contains(info.getX(), info.getY()); //ist der Punkt im Hinderniss?
-
-        int numberofObstacles = obs.npoints; //Anzahl der Punkte des Hindernisses
-        //A = obs.xpoints[0], obs.ypoints[0];
-        //B = obs.xpoints[1], obs.ypoints[1]; //flee to get away from these points, collison avoidance viel besser
-        //Erstelle Streck von A und B
-        //Erstelle Richtungsvektor
-        info.getVelocity();
-        //Berechne Schnittpunkt der beiden obigen, pruefe Abstand
-
-
-
         track.getObstacles(); // Hindernisse - nächste Übung
 
 
-        float drehbeschleunigungVonAlign = 0.1f;
-        return new DriverAction(1, drehbeschleunigungVonAlign);
+        /**
+         *
+         * BESCHLEUNIGUNG
+         *
+         */
+
+        float breakRadius = 30;
+        float checkpointRadius = 1f;
+        float speed;
+
+        if( MathVec.abs(carToCheckpoint) < breakRadius) {
+            speed = MathVec.abs(carToCheckpoint) * info.getMaxVelocity() / breakRadius / 4;
+            if ( MathVec.abs(carToCheckpoint) < checkpointRadius) {
+                Vec2 currentSpeedVector = new Vec2(info.getVelocity().x, info.getVelocity().y);
+                float currentSpeed = MathVec.abs(currentSpeedVector);
+                accelaration = info.getMaxVelocity() - currentSpeed / 1;
+                accelaration = (accelaration > info.getMaxVelocity()) ? info.getMaxVelocity() : accelaration;
+                speed = accelaration;
+            }
+        } else {
+            speed = info.getMaxVelocity();
+        }
+        return new DriverAction(speed, absoluteSteeringStrength);
+
     }
+
 
     @Override
     public String getTextureResourceName() {
